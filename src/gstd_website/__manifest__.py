e449d57087410005cb222c73fd58dff75ae38042
edgardelacruzv <ecruz@globalstd.com>
2017-10-19 09:02:52 -0500

    first commit

---
+++ src/gstd_website/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'GlobalStd Complaints Website',
+    'version' : '1.0',
+    'summary': 'GlobalStd Complaints Website',
+    'sequence': 30,
+    'description': '',
+    'category': 'website',
+    'author': 'GlobalSTD',
+    'website': 'http://www.globalstd.com/',
+    'images' : [],
+    'depends' : ['website'],
+    'data': [
+        'views/layout.xml',
+        'views/index.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
